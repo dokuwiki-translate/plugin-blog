eb36594c1915f0a568912d06336782c83bf2a79b
Julio García <yllelder@gmail.com>
2021-10-22 23:00:36 +0200

    translation update

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -2,8 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author     Herman Fabián Sandoval Manrique <hfsandovalm@emzac.com>
+ *
+ * @author Julio García <yllelder@gmail.com>
+ * @author Herman Fabián Sandoval Manrique <hfsandovalm@emzac.com>
  * @author r0sk <r0sk10@gmail.com>
  */
 $lang['namespace']             = 'namespace predeterminado para el blog';
@@ -12,12 +13,17 @@ $lang['formposition_o_top']    = 'arriba';
 $lang['formposition_o_bottom'] = 'abajo';
 $lang['formposition_o_none']   = 'ninguna';
 $lang['newentrytitle']         = 'título del formulario de nueva entrada';
+$lang['dateprefix']            = 'prefijo de fecha para los nuevos ID de entrada';
 $lang['sortkey']               = 'ordenar entradas por';
 $lang['sortkey_o_cdate']       = 'fecha de creación';
 $lang['sortkey_o_mdate']       = 'fecha de modificación';
 $lang['sortkey_o_pagename']    = 'nombre de página';
 $lang['sortkey_o_id']          = 'ID de página';
 $lang['sortkey_o_title']       = 'título';
-$lang['sortorder']             = 'ordenamiento';
+$lang['sortorder']             = 'orden';
 $lang['sortorder_o_ascending'] = 'ascendente';
 $lang['sortorder_o_descending'] = 'descendente';
+$lang['excluded_pages']        = 'excluir ciertas páginas del listado del blog (se requiere una expresión regular)';
+$lang['showhistogram']         = 'Mostrar el histograma en la representación de archivos';
+$lang['max_months']            = 'Máximo de meses a mostrar en el histograma';
+$lang['histogram_height']      = 'Altura del histograma (en píxeles)';
